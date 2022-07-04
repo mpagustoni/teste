8fbccb2095df0c1497cf551c50de2545514f1dd2
INFOAR\mauricio.agustoni <mauricio.agustoni@infoar.com.br>
2022-07-04 10:09:00 -0300

    novo commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+batata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata fritabatata frita
+batata fritabatata fritabatata fritabatata fritabatata frita
+batata fritabatata fritabatata fritabatata frita
+batata fritabatata fritabatata fritabatata frita
+batata fritabatata frita
 batata frita
+batata frita
+v
+v
